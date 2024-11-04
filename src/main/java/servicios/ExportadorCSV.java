b747a91370365804384879e8d4ddb69d5a9f5174
Luisda <luisda915@gmail.com>
2024-11-04 20:43:17 -0300

    creando el metodo para exportar csv

--- src/main/java/servicios/ExportadorCSV.java
+++ src/main/java/servicios/ExportadorCSV.java
@@ -1,5 +1,28 @@
 package servicios;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import com.opencsv.CSVWriter;
+
+import modelo.Proyecto;
+import modelo.Tarea;
+
 public class ExportadorCSV {
+    public void exportarTareas(List<Proyecto> proyectos) throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter("tareas.csv"))) {
+            for (Proyecto proyecto : proyectos) {
+                for (Tarea tarea : proyecto.getTareas()) {
+                    String[] linea = {
+                        proyecto.getNombre(),
+                        tarea.getDescripcion(),
+                        tarea.getAsignado().getNombreE()
+                    };
+                    writer.writeNext(linea);
+                }
+            }
+        }
+    }
 
 }
